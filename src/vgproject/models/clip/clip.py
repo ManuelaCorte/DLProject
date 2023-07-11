9c7c5e05733cd307a254681d418660a3808aad61
ManuelaCorte <manu.cortepause@gmail.com>
2023-07-11 14:56:43 +0200

    Modified clip backbone: - returns ResNet intermediate layers - modified ResNet attention pooling - returns transformer output

---
+++ src/vgproject/models/clip/clip.py
@@ -0,0 +1,91 @@
+from typing import Union, List
+import warnings
+import pkg_resources as p
+
+import torch
+from PIL import Image
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+
+from .simple_tokenizer import SimpleTokenizer as _Tokenizer
+
+try:
+    from torchvision.transforms import InterpolationMode
+
+    BICUBIC = InterpolationMode.BICUBIC
+except ImportError:
+    BICUBIC = Image.BICUBIC
+
+
+if p.parse_version(torch.__version__) < p.parse_version("1.7.1"):
+    warnings.warn("PyTorch version 1.7.1 or higher is recommended")
+
+
+_tokenizer = _Tokenizer()
+
+
+def _convert_image_to_rgb(image) -> Image.Image:
+    return image.convert("RGB")
+
+
+def transform(n_px) -> Compose:
+    return Compose(
+        [
+            Resize(n_px, interpolation=BICUBIC),
+            CenterCrop(n_px),
+            _convert_image_to_rgb,
+            ToTensor(),
+            Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
+
+
+def tokenize(
+    texts: Union[str, List[str]], context_length: int = 77, truncate: bool = False
+) -> Union[torch.IntTensor, torch.LongTensor]:
+    """
+    Returns the tokenized representation of given input string(s)
+
+    Parameters
+    ----------
+    texts : Union[str, List[str]]
+        An input string or a list of input strings to tokenize
+
+    context_length : int
+        The context length to use; all CLIP models use 77 as the context length
+
+    truncate: bool
+        Whether to truncate the text in case its encoding is longer than the context length
+
+    Returns
+    -------
+    A two-dimensional tensor containing the resulting tokens, shape = [number of input strings, context_length].
+    We return LongTensor when torch version is <1.8.0, since older index_select requires indices to be long.
+    """
+    if isinstance(texts, str):
+        texts = [texts]
+
+    sot_token = _tokenizer.encoder["<|startoftext|>"]
+    eot_token = _tokenizer.encoder["<|endoftext|>"]
+    all_tokens = [[sot_token] + _tokenizer.encode(text) + [eot_token] for text in texts]
+
+    result: Union[torch.IntTensor, torch.LongTensor]
+    if p.parse_version(torch.__version__) < p.parse_version("1.8.0"):
+        result = torch.zeros(len(all_tokens), context_length, dtype=torch.long)  # type: ignore
+    else:
+        result = torch.zeros(len(all_tokens), context_length, dtype=torch.int)  # type: ignore
+
+    for i, tokens in enumerate(all_tokens):
+        if len(tokens) > context_length:
+            if truncate:
+                tokens = tokens[:context_length]
+                tokens[-1] = eot_token
+            else:
+                raise RuntimeError(
+                    f"Input {texts[i]} is too long for context length {context_length}"
+                )
+        result[i, : len(tokens)] = torch.tensor(tokens)
+
+    return result
