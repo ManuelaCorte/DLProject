8a50bebd0e7a49d8c4122c8bbb6f4d261fb6800a
ManuelaCorte <manu.cortepause@gmail.com>
2023-07-17 13:04:22 +0200

    Changed optimizer

--- src/vgproject/utils/visualization.py
+++ src/vgproject/utils/visualization.py
@@ -1,14 +1,16 @@
-from typing import List
+from typing import List, Tuple
 
 import torch
 from matplotlib import pyplot as plt
-from torch import Tensor
+from torch import Tensor, nn, optim
 from torch.utils.data import DataLoader
+from torch_lr_finder import LRFinder  # type: ignore
 from torchvision.io import read_image
 from torchvision.utils import draw_bounding_boxes
 from torchviz import make_dot  # type: ignore
 
 from vgproject.data.dataset import VGDataset
+from vgproject.metrics.loss import Loss
 from vgproject.models.vg_model.vg_model import VGModel
 from vgproject.utils.misc import custom_collate
 
@@ -62,33 +64,56 @@ def visualize_network(model: torch.nn.Module, batch: List[BatchSample]) -> None:
     ).render("model_graph", directory="../runs", format="png")
 
 
-if __name__ == "__main__":
-    cfg = Config()
-    dataset = VGDataset(
-        dir_path=cfg.dataset_path,
-        split=Split.TEST,
-        output_bbox_type=BboxType.XYXY,
+def find_lr() -> None:
+    config = Config()
+    model = VGModel(config).train()
+
+    loss_func = Loss(config.train.l1, config.train.l2)
+
+    # Separate parameters to train
+    backbone_params: List[nn.Parameter] = [
+        p for p in model.pretrained_model.parameters() if p.requires_grad
+    ]
+
+    # All parameters except the backbone
+    non_frozen_params: List[nn.Parameter] = [
+        p for p in model.fusion_module.parameters()
+    ]
+    non_frozen_params.extend(model.decoder.parameters())
+    non_frozen_params.extend(model.reg_head.parameters())
+
+    optimizer = optim.Adam(
+        [
+            {"params": backbone_params, "lr": config.train.lr_backbone},
+            {"params": non_frozen_params, "lr": config.train.lr},
+        ]
+    )
+    train_dataset: VGDataset = VGDataset(
+        dir_path=config.dataset_path,
+        split=Split.TRAIN,
+        output_bbox_type=BboxType.XYWH,
         augment=True,
         preprocessed=True,
     )
-    dataloader = DataLoader(
-        dataset,
-        batch_size=cfg.train.batch_size,
+    train_dataloader: DataLoader[Tuple[BatchSample, Tensor]] = DataLoader(
+        dataset=train_dataset,
+        batch_size=config.train.batch_size,
         collate_fn=custom_collate,
+        num_workers=2,
         shuffle=True,
         drop_last=True,
     )
-    samples = dataset.samples[:6]
-    print(samples[0].bounding_box, samples[0].bounding_box.shape)
-    predictions = torch.stack(
-        [sample.bounding_box for sample in dataset.samples[14:20]]
+    lr_finder = LRFinder(
+        model,
+        optimizer,
+        loss_func,
+        device=torch.device("cuda") if torch.cuda.is_available() else "cpu",
     )
-    visualize(samples, predictions)
+    lr_finder.range_test(train_dataloader, end_lr=100, num_iter=100, diverge_th=20)
+    f, ax = plt.subplots(1)
+    lr_finder.plot(suggest_lr=True, ax=ax)  # to inspect the loss-learning rate graph
+    plt.show()
 
-    cfg = Config()
-    test = VGModel(cfg)
-    for batch, bbox in dataloader:
-        # print(batch[0].image, batch[0].caption)
-        out = test(batch)
-        visualize_network(test, batch)
-        break
+
+if __name__ == "__main__":
+    find_lr()
