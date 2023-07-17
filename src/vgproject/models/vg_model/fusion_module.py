Fixed normalization layers

src/vgproject/models/vg_model/fusion_module.py
@@ -10,7 +10,8 @@ class FusionModule(nn.Module):
         super().__init__()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.text_projection = nn.Sequential(
-            nn.Linear(in_features=clip_emb_dim, out_features=clip_emb_dim), nn.ReLU()
+            nn.Linear(in_features=clip_emb_dim, out_features=clip_emb_dim),
+            nn.BatchNorm1d(clip_emb_dim, device=self.device),
         ).to(self.device)
 
         self.vis_l4_projection = _conv_layer(
@@ -19,10 +20,13 @@ class FusionModule(nn.Module):
             kernel_size=1,
             padding=0,
             device=self.device,
-        )
+        )[
+            :2
+        ]  # Remove ReLU
         self.norm_layer = nn.Sequential(
-            nn.LayerNorm(
-                [1024, proj_img_size // 2, proj_img_size // 2], device=self.device
+            nn.BatchNorm2d(
+                clip_emb_dim,
+                device=self.device,
             ),
             nn.ReLU(),
         ).to(self.device)
@@ -53,7 +57,7 @@ class FusionModule(nn.Module):
     ) -> Tensor:
         visual_l2_features, visual_l3_features, visual_l4_features = visual_features
         # Visual and text features projection
-        text_features_proj = (
+        text_features_proj: Tensor = (
             self.text_projection(text_features).unsqueeze(-1).unsqueeze(-1)
         )  # B 1024 1 1
         visual_l4_features_proj: Tensor = self.vis_l4_projection(
@@ -69,22 +73,24 @@ class FusionModule(nn.Module):
         fused_l4_upsample: Tensor = nn.Upsample(scale_factor=2, mode="nearest")(
             fused_l4
         )  # B 1024 14 14
-        cat_features = torch.cat([visual_l3_features, fused_l4_upsample], dim=1)
+        cat_features: Tensor = torch.cat([visual_l3_features, fused_l4_upsample], dim=1)
         fused_l3: Tensor = self.vis_l3_projection(cat_features)  # B 512 14 14
 
         # Third fusion l2 (B 512 28 28) and l3 (B 512 14 14)
         visual_l2_pooling = nn.MaxPool2d(kernel_size=2, stride=2)(
             visual_l2_features
         )  # B 512 14 14
-        fused_l2 = self.vis_l2_projection(
+        fused_l2: Tensor = self.vis_l2_projection(
             torch.cat([fused_l3, visual_l2_pooling], dim=1)
         )  # B 512 14 14
 
         # Aggregate features
-        cat_visual_features = torch.cat(
+        cat_visual_features: Tensor = torch.cat(
             [fused_l2, fused_l3, fused_l4_upsample], dim=1
         )  # B 2048 14 14
-        aggregated_features = self.aggregation(cat_visual_features)  # B 512 14 14
+        aggregated_features: Tensor = self.aggregation(
+            cat_visual_features
+        )  # B 512 14 14
         # TODO: Add spatial coords?
         return aggregated_features
 