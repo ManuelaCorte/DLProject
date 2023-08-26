Minor fixes: - bugfix in bounding boxes normalization - changed final activation function

src/vgproject/eval.py
@@ -21,7 +21,7 @@ from vgproject.utils.misc import custom_collate
 
 
 @torch.no_grad()
-def eval() -> None:
+def eval(model_file: str) -> None:
     cfg = Config()
     dataset = VGDataset(
         dir_path=cfg.dataset_path,
@@ -41,7 +41,9 @@ def eval() -> None:
     )
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    checkpoint: Dict[str, Any] = torch.load("../model0.pth", map_location=device)
+    checkpoint: Dict[str, Any] = torch.load(
+        f"../runs/{model_file}", map_location=device
+    )    
     model: VGModel = VGModel(cfg)
     model.load_state_dict(checkpoint["model_state_dict"])
     model.eval()
@@ -174,4 +176,4 @@ def accuracy(iou: Tensor, threshold: float) -> Tensor:
 
 
 if __name__ == "__main__":
-    eval()
+    eval("model0.pth")

src/vgproject/models/vg_model/vg_model.py
@@ -53,8 +53,14 @@ class VGModel(nn.Module):
             cfg.model.decoder_dim_feedforward,
         ).to(self.device)
 
+        activation: nn.Module = (
+            nn.Sigmoid() if cfg.model.activation == "sigmoid" else nn.Softplus()
+        )
         self.reg_head: MLP = MLP(
-            input_dim=embed_dim, output_dim=4, hidden_dim_1=mlp_hidden_dim
+            input_dim=embed_dim,
+            output_dim=4,
+            hidden_dim_1=mlp_hidden_dim,
+            act_func=activation,
         ).to(self.device)
 
     def forward(self, batch: List[BatchSample]) -> Tensor:
@@ -82,13 +88,16 @@ class VGModel(nn.Module):
 
 
 class MLP(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int, hidden_dim_1: int) -> None:
+    def __init__(
+        self, input_dim: int, output_dim: int, hidden_dim_1: int, act_func: nn.Module
+    ) -> None:
         super().__init__()
         self.mlp = nn.Sequential(
             nn.Linear(input_dim, hidden_dim_1),
             nn.BatchNorm1d(hidden_dim_1),
             nn.ReLU(),
             nn.Linear(hidden_dim_1, output_dim),
+            act_func,
         )
 
     def forward(self, x: Tensor) -> Tensor:

src/vgproject/utils/engines.py
@@ -42,8 +42,8 @@ def train_one_epoch(
         out: Tensor = model(batch)
 
         # Loss and metrics
-        out_xyxy = box_convert(out * img_size, in_fmt="xywh", out_fmt="xyxy")
-        bbox_xyxy = box_convert(bbox * img_size, in_fmt="xywh", out_fmt="xyxy")
+        out_xyxy = box_convert(out, in_fmt="xywh", out_fmt="xyxy")
+        bbox_xyxy = box_convert(bbox, in_fmt="xywh", out_fmt="xyxy")
         batch_loss: Tensor = loss.compute(out_xyxy, bbox_xyxy)
 
         # Backward pass
@@ -106,8 +106,8 @@ def validate(
         # Forward pass
         out: Tensor = model(batch)
 
-        out = box_convert(out * img_size, in_fmt="xywh", out_fmt="xyxy").detach()
-        bbox = box_convert(bbox * img_size, in_fmt="xywh", out_fmt="xyxy").detach()
+        out = box_convert(out, in_fmt="xywh", out_fmt="xyxy").detach()
+        bbox = box_convert(bbox, in_fmt="xywh", out_fmt="xyxy").detach()
 
         batch_loss: Tensor = loss.compute(out, bbox).detach()
         batch_iou: Tensor = torch.diagonal(box_iou(out, bbox)).detach()